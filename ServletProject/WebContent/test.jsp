f6cf84af91c909819372c7cb2c1f114c1f7f758c
301 <301@223.194.169.106>
2015-03-27 16:48:32 +0900

    0327

---
+++ ServletProject/WebContent/test.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+    
+    <html>
+    <head>
+    	<title>JSP/Servlet Example</title>
+    	<style>
+    		/*이게 주석입니다. //이거 안됩니다.*/
+    		body{
+    			background-image:url("pic.jpg.png");
+    			background-repeat:no-repeat;
+    			background-attachment: fixed;
+    		}
+    		input, select, textarea{
+    			border:green 2pt solid;
+    			color:red;
+    			font-size:10px;
+    		}
+    	
+    	</style>
+    	<script>
+    		function gofunc (){
+    			
+    			alert("Hello JavaScript!");
+    		}
+    	
+			    	
+    	</script>
+    </head>
+    
+    <body>
+    <h1>request test</h1>
+    <hr>
+    
+    <form action="TestServlet" onsubmit="return gofunc()"> <!--  submit버튼을 눌렀을때 위에 자바스크립트 함수를 호출-->
+    	<label for="label_name">이름 : </label><input type="text" id="label_name" name="t">
+    	<br>
+    	<label for="label_pwd">비밀번호 : </label><input type="password" id="label_pwd"name="p">
+    	<br>
+    	<input type="radio" name="gender" value="남자"> 남자
+    	<input type="radio" name="gender" value="여자"> 여자 <br>
+    	
+    	<input type="checkbox" name="ball" value="축구"> 축구
+    	<input type="checkbox" name="ball" value="야구"> 야구
+    	<input type="checkbox" name="ball" value="농구"> 농구<br>
+    	
+    	<select name="lang">
+    		<option value="java">Java</option>
+    		<option value="JSP">JSP</option>
+    		<option value="html">HTML</option>
+    	</select><br>
+    	
+    	<!--  라디오 버튼은 name이 같을때 다를때 효과가 다르지만, 체크박스는 아님 -->
+
+    	<input type="color" name="mycolor">
+    	E-Mail : <input type="email" name="email">
+
+		<textarea rows="20" cols="30" name="tx">
+		
+		</textarea>    	
+    	
+    	
+    	<input type="submit" value="서버로 제출">
+
+    </form>
+    </body>
+    </html>
---
+++ ServletProject/src/exam/test/TestServlet.java
@@ -0,0 +1,84 @@
+package exam.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");	//<-이게 있어야 BR태그가 됨, 한글이 안깨짐
+		String n = request.getParameter("t");	//넘어오는 이름을 맞춰야되
+		
+		PrintWriter out = response.getWriter();	//내장객체가 없기 때문에 writer라는 별도의 메소드를 사용해야됨
+		out.println(n);
+		
+		String m = request.getParameter("p");	//request는 리턴타입이 String
+		String s = request.getParameter("gender");
+		//getParameter를 써서 submit버튼을 누르면 값이 넘어오게함
+		out.println(m);
+		out.println("gender");
+		
+		String color = request.getParameter("mycolor");
+		
+		out.println("<font color='"+color+"'>"+color+"</font>");	//font color 속성에 color라는 변수를 넣어줌
+		
+		String mail = request.getParameter("email");
+		
+		out.println(mail);
+		
+		String[] hobby = request.getParameterValues("ball"); //리턴타입이 String 배열
+		
+		if(hobby == null)
+			out.println("선택을 하고 누르세요");
+		else{
+			out.println(hobby.length + "개 선택!");
+			
+			for(int i=0; i<hobby.length;i++)
+				out.println(hobby[i] + "<br>");
+			
+		}
+		
+		String subject = request.getParameter("lang");
+		out.println(subject+"<br>");
+		
+		String area = request.getParameter("tx");
+		out.println(area+"<br>");
+		
+		//값 하나만 넘어오는거는 getParameter!!!!!
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
